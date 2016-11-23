17c78c3f27930765af5f22dc959f261eed1c4bb5
Gabe <rosati.gabriel@gmail.com>
2016-11-23 11:03:35 -0800

    Add pomodoro

--- only-built.sh
+++ only-built.sh
@@ -1,3 +1,4 @@
 ln -s $HOME/Config/init.el $HOME/.emacs.d/init.el
 ln -s $HOME/Config/.bashrc $HOME/.bashrc
+ln -s $HOME/Config/pom $HOME/bin/pom
 rm $HOME/.bash_profile && echo "source $HOME/.bashrc" > $HOME/.bash_profile
