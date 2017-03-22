e89fcc35e76644aafeb34c056c79c2afa672f9ad
Gabe <gbrl@tuta.io>
2017-03-22 10:19:31 -0700

    Refine and rename setups

--- ba.sh
+++ ba.sh
@@ -5,7 +5,12 @@ alias notes="cd $HOME/Notes/"
 alias movgif="ffmpeg -i in.mov -s 600x400 -r 10 -f gif - | gifsicle --optimize=3 --delay=3 > out.gif"
 alias hgee="hg branches && hg status"
 
-alias wot="grep -e '^alias' $HOME/.bashrc && grep -e '^alias' $HOME/Config/bash"
+function wot() {
+    grep -e '^alias ' $HOME/.bashrc
+    grep -e '^alias ' $HOME/Config/ba.sh
+    awk '/function /,/}$/' $HOME/.bashrc
+    awk '/function /,/}$/' $HOME/Config/ba.sh
+}
 
 function dis() {
     source $HOME/Config/setd.sh
