989655c4d8b7d6e6834bd16b48b848397a4095f5
Gabe <gbrl@tuta.io>
2017-04-01 04:17:05 -0700

    Add local.el, i3status config, and mkdirs to fre.sh

--- fre.sh
+++ fre.sh
@@ -1,3 +1,12 @@
 printf "source $HOME/Config/ba.sh\n" >> $HOME/.bashrc
+
+mkdir $HOME/.emacs.d/
+touch $HOME/.emacs.d/local.el
 ln -s $HOME/Config/init.el $HOME/.emacs.d/init.el
+
+mkdir -p $HOME/.config/i3/
 ln $HOME/Config/i3-config $HOME/.config/i3/config
+
+mkdir -p $HOME/.config/i3status/
+ln $HOME/Config/i3-status-config $HOME/.config/i3status/config
+
